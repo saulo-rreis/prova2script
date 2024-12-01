0e083f75751955b7e5c0212e7c3a2e8b1232f120
saulo-rreis <83778133+saulo-rreis@users.noreply.github.com>
2024-11-30 23:22:56 -0300

    Create CartDetails.vue

---
+++ src/components/CartDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- Seu HTML aqui -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartDetails',
+  data() {
+    return {
+      // Seus dados aqui
+    };
+  },
+  methods: {
+    // Seus métodos aqui
+  },
+};
+</script>
+
+<style scoped>
+/* Seus estilos aqui */
+</style>
